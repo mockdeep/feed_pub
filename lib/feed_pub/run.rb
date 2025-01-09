09c002dfbc387b5be1fa59d62bb8e8f3e0107691
Robert Fletcher <lobatifricha@gmail.com>
2025-01-09 13:15:03 -0800

    remove headers
    doesn't appear to be necessary, at least for now

--- lib/feed_pub/run.rb
+++ lib/feed_pub/run.rb
@@ -11,12 +11,10 @@ module FeedPub::Run
       image_selector = infer_image_selector(session)
       next_selector = infer_next_selector(session)
       puts "Image selector: '#{image_selector}', Next selector: '#{next_selector}'"
-      user_agent = session.evaluate_script('navigator.userAgent')
-      headers = { "User-Agent" => user_agent }
 
       # need to number the images in case they don't have sequenced names
       sequence = "00000"
-      download_image(session.find(image_selector)["src"], sequence:, headers:)
+      download_image(session.find(image_selector)["src"], sequence:)
 
       while session.has_css?(next_selector) && Integer(sequence, 10) < max_pages
         current_url = session.current_url
@@ -32,7 +30,7 @@ module FeedPub::Run
         sequence = sequence.next
         # break if sequence > "00010" # for testing purposes
 
-        download_image(session.find(image_selector)["src"], sequence:, headers:)
+        download_image(session.find(image_selector)["src"], sequence:)
       end
 
       # merge images into a single PDF
@@ -83,7 +81,7 @@ module FeedPub::Run
       end
     end
 
-    def download_image(image_url, sequence:, headers:)
+    def download_image(image_url, sequence:)
       return if processed_urls.include?(image_url)
 
       filename = "#{sequence}_#{File.basename(image_url)}"
@@ -92,7 +90,7 @@ module FeedPub::Run
       # if they do, we'll need to adjust our algorithm
       raise "File already exists: #{filename}" if File.exist?(filename)
 
-      response = HTTP.follow.get(image_url, headers:)
+      response = HTTP.follow.get(image_url)
 
       File.write(filename, response.body)
       File.write(PROCESSED_URLS, "#{image_url}\n", mode: "a")
