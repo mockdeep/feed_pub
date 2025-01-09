166658c6076b4b234d84e67f627fa1cc3abf8fc3
Robert Fletcher <lobatifricha@gmail.com>
2025-01-09 12:31:49 -0800

    use user agent for HTTP call

--- lib/feed_pub/run.rb
+++ lib/feed_pub/run.rb
@@ -10,11 +10,13 @@ module FeedPub::Run
       next_selector = ".next-button"
 
       session = Capybara::Session.new(:selenium)
+      user_agent = session.evaluate_script('navigator.userAgent')
+      headers = { "User-Agent" => user_agent }
       session.visit(url)
 
       # need to number the images in case they don't have sequenced names
       sequence = "00000"
-      download_image(session.find(image_selector)["src"], sequence:)
+      download_image(session.find(image_selector)["src"], sequence:, headers:)
 
       while session.has_css?(next_selector) && Integer(sequence, 10) < max_pages
         current_url = session.current_url
@@ -30,7 +32,7 @@ module FeedPub::Run
         sequence = sequence.next
         # break if sequence > "00010" # for testing purposes
 
-        download_image(session.find(image_selector)["src"], sequence:)
+        download_image(session.find(image_selector)["src"], sequence:, headers:)
       end
 
       # merge images into a single PDF
@@ -42,7 +44,7 @@ module FeedPub::Run
 
     private
 
-    def download_image(image_url, sequence:)
+    def download_image(image_url, sequence:, headers:)
       return if processed_urls.include?(image_url)
 
       filename = "#{sequence}_#{File.basename(image_url)}"
@@ -51,7 +53,9 @@ module FeedPub::Run
       # if they do, we'll need to adjust our algorithm
       raise "File already exists: #{filename}" if File.exist?(filename)
 
-      File.write(filename, HTTP.get(image_url))
+      response = HTTP.get(image_url, headers:)
+
+      File.write(filename, response.body)
       File.write(PROCESSED_URLS, "#{image_url}\n", mode: "a")
     end
 
