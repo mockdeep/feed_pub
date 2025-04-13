bbe315451d1b8bd707b92136583348ced47c2d47
Robert Fletcher <lobatifricha@gmail.com>
2025-04-12 22:09:35 -0700

    eliminate file stubbing in run_spec (#26)

--- spec/feed_pub/run_spec.rb
+++ spec/feed_pub/run_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe FeedPub::Run do
     File.join(Dir.pwd, "tmp")
   end
 
-  def processed_path
-    File.join(filepath, "downloaded_images.txt")
+  def tempfiles
+    Dir.glob(File.join(filepath, "*")).map { |f| File.basename(f) }
   end
 
   after do
@@ -25,58 +25,37 @@ RSpec.describe FeedPub::Run do
 
   it "downloads images and creates a pdf" do
     stub_request(:get, "https://foo.png").to_return(body: "image data")
-    image_path = File.join(filepath, "00000_foo.png")
-    expect(File).to receive(:write).with(image_path, "image data")
-    expect(File).to receive(:write).with(
-      processed_path,
-      "https://foo.png\n",
-      mode: "a",
-    )
-    expect(File).to receive(:delete).with(processed_path)
     expect(described_class).to receive(:`).with("convert * comic.pdf")
 
     described_class.call("one_image", output: StringIO.new, filepath:)
+
+    expect(tempfiles).to eq(["00000_foo.png"])
   end
 
   it "clicks the next link and downloads images from each page" do
     stub_request(:get, "https://foo.png").to_return(body: "image data")
     stub_request(:get, "https://bar.png").to_return(body: "image data")
-    image_path_1 = File.join(filepath, "00000_foo.png")
-    expect(File).to receive(:write).with(image_path_1, "image data")
-    image_path_2 = File.join(filepath, "00001_bar.png")
-    expect(File).to receive(:write).with(image_path_2, "image data")
-    expect(File).to receive(:write).with(
-      processed_path,
-      "https://foo.png\n",
-      mode: "a",
-    )
-    expect(File).to receive(:write).with(
-      processed_path,
-      "https://bar.png\n",
-      mode: "a",
-    )
-    expect(File).to receive(:delete).with(processed_path)
     expect(described_class).to receive(:`).with("convert * comic.pdf")
 
     described_class.call("next_link", output: StringIO.new, filepath:)
+
+    expect(tempfiles).to eq(["00000_foo.png", "00001_bar.png"])
   end
 
   it "does not download images when already downloaded" do
-    File.write(processed_path, "https://foo.png\n")
-
     stub_request(:get, "https://foo.png").to_return(body: "image data")
-    expect(File).not_to receive(:write)
-    expect(File).to receive(:delete).with(processed_path)
     expect(described_class).to receive(:`).with("convert * comic.pdf")
 
-    described_class.call("one_image", output: StringIO.new, filepath:)
+    described_class.call("duplicate_image", output: StringIO.new, filepath:)
+
+    expect(tempfiles).to eq(["00000_foo.png"])
   end
 
   it "raises an error when image file already exists" do
     stub_request(:get, "https://foo.png").to_return(body: "image data")
-    allow(File).to receive(:exist?).with(processed_path).and_return(false)
     image_path = File.join(filepath, "00000_foo.png")
-    allow(File).to receive(:exist?).with(image_path).and_return(true)
+    File.write(image_path, "image data")
+
     expect { described_class.call("image_class", output: StringIO.new, filepath:) }
       .to raise_error("File already exists: 00000_foo.png")
   end
