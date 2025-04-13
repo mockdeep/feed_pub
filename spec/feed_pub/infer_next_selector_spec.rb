e79fca3ac9a421565df7508c3d34e7de2f788bf3
Robert Fletcher <lobatifricha@gmail.com>
2025-04-12 20:57:27 -0700

    replace TestSession references (#25)

--- spec/feed_pub/infer_next_selector_spec.rb
+++ spec/feed_pub/infer_next_selector_spec.rb
@@ -2,16 +2,13 @@
 
 RSpec.describe FeedPub::InferNextSelector do
   it "returns a link selector when a link with 'Next' is found" do
-    session = TestSession.new
-    expect(session).to receive(:has_link?).with("Next").and_return(true)
+    session = TestNode.new("<a href='/foo'>Next</a>")
 
     expect(described_class.call(session, output: StringIO.new)).to be_a(FeedPub::InferNextSelector::LinkSelector)
   end
 
   it "returns an alt selector when an element with 'next' in alt is found" do
-    session = TestSession.new
-    element = Capybara.string("<button alt='next'></button>").find("button")
-    allow(session).to receive(:all).and_return([element])
+    session = TestNode.new("<button alt='next'></button>")
 
     selector = described_class.call(session, output: StringIO.new)
 
@@ -19,9 +16,7 @@ RSpec.describe FeedPub::InferNextSelector do
   end
 
   it "returns a class selector when an element with 'next' in class is found" do
-    session = TestSession.new
-    element = Capybara.string("<button class='next'></button>").find("button")
-    allow(session).to receive(:all).and_return([element])
+    session = TestNode.new("<button class='next'></button>")
 
     selector = described_class.call(session, output: StringIO.new)
 
@@ -31,17 +26,15 @@ RSpec.describe FeedPub::InferNextSelector do
   describe FeedPub::InferNextSelector::LinkSelector do
     describe "#matches?" do
       it "returns true when the link is found" do
-        session = TestSession.new
-        allow(session).to receive(:has_link?).and_return(true)
         selector = described_class.new("Next")
+        session = TestNode.new("<a href='/foo'>Next</a>")
 
         expect(selector.matches?(session)).to be(true)
       end
 
       it "returns false when the link is not found" do
-        session = TestSession.new
-        allow(session).to receive(:has_link?).and_return(false)
         selector = described_class.new("Next")
+        session = TestNode.new("<a href='/foo'>Foo</a>")
 
         expect(selector.matches?(session)).to be(false)
       end
@@ -49,9 +42,7 @@ RSpec.describe FeedPub::InferNextSelector do
 
     describe "#click" do
       it "clicks the link" do
-        session = TestSession.new
-        element = Capybara.string("<a href='next'></a>").find("a")
-        allow(session).to receive(:find_link).and_return(element)
+        session = TestNode.new("<a href='next'></a>")
         expect(session).to receive(:visit).with("next")
 
         described_class.new("").click(session)
