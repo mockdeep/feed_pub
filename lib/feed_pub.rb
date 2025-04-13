8f27793704ca27228b46e9a1757f0fdf41e88100
Robert Fletcher <lobatifricha@gmail.com>
2025-04-12 22:20:56 -0700

    Rubocop: fix quote violations (#28)

--- lib/feed_pub.rb
+++ lib/feed_pub.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'capybara'
-require 'active_support/all'
-require 'http'
+require "capybara"
+require "active_support/all"
+require "http"
 
 module FeedPub
 end
