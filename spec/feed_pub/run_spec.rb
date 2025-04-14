e3b78a840b077291d60a87c8aead31be6a67badf
Robert Fletcher <lobatifricha@gmail.com>
2025-04-13 17:06:23 -0700

    switch pngs to jpgs

--- spec/feed_pub/run_spec.rb
+++ spec/feed_pub/run_spec.rb
@@ -17,36 +17,35 @@ RSpec.describe FeedPub::Run do
   end
 
   it "downloads images and creates a pdf" do
-    stub_request(:get, "https://foo.png").to_return(body: sketch)
+    stub_request(:get, "https://foo.jpg").to_return(body: sketch)
 
     described_class.call("one_image", output: StringIO.new, file_path:)
 
-    expect(tempfile_names).to eq(["00000_foo.png", "comic.pdf"])
+    expect(tempfile_names).to eq(["00000_foo.jpg", "comic.pdf"])
   end
 
   it "clicks the next link and downloads images from each page" do
-    stub_request(:get, "https://foo.png").to_return(body: sketch)
-    stub_request(:get, "https://bar.png").to_return(body: sketch)
+    stub_request(:get, "https://foo.jpg").to_return(body: sketch)
+    stub_request(:get, "https://bar.jpg").to_return(body: sketch)
 
     described_class.call("next_link", output: StringIO.new, file_path:)
 
-    expect(tempfile_names).to eq(["00000_foo.png", "00001_bar.png", "comic.pdf"])
+    expect(tempfile_names).to eq(["00000_foo.jpg", "00001_bar.jpg", "comic.pdf"])
   end
 
   it "does not download images when already downloaded" do
-    stub_request(:get, "https://foo.png").to_return(body: sketch)
+    stub_request(:get, "https://foo.jpg").to_return(body: sketch)
 
     described_class.call("duplicate_image", output: StringIO.new, file_path:)
 
-    expect(tempfile_names).to eq(["00000_foo.png", "comic.pdf"])
+    expect(tempfile_names).to eq(["00000_foo.jpg", "comic.pdf"])
   end
 
   it "raises an error when image file already exists" do
-    stub_request(:get, "https://foo.png").to_return(body: sketch)
-    image_path = File.join(file_path, "00000_foo.png")
-    File.write(image_path, sketch)
+    stub_request(:get, "https://foo.jpg").to_return(body: sketch)
+    File.write(File.join(file_path, "00000_foo.jpg"), sketch)
 
     expect { described_class.call("image_class", output: StringIO.new, file_path:) }
-      .to raise_error("File already exists: 00000_foo.png")
+      .to raise_error("File already exists: 00000_foo.jpg")
   end
 end
