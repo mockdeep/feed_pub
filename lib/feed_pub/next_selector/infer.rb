355e52a99603c22300eb4c263c76fd3332f8e288
Robert Fletcher <lobatifricha@gmail.com>
2025-04-13 21:39:34 -0700

    convert NextSelector::Infer to strategy pattern

--- lib/feed_pub/next_selector/infer.rb
+++ lib/feed_pub/next_selector/infer.rb
@@ -1,30 +1,22 @@
 # frozen_string_literal: true
 
 module FeedPub::NextSelector::Infer
+  SELECTORS = [
+    FeedPub::NextSelector::Link.new("Next"),
+    FeedPub::NextSelector::Attribute.new("[id*='next']"),
+    FeedPub::NextSelector::Attribute.new("[class*='next']"),
+    FeedPub::NextSelector::Attribute.new("[alt*='next']"),
+  ].freeze
+
   class << self
     include FeedPub::Helpers
 
     def call(session)
       output.puts "inferring next selector"
-      if session.has_link?("Next")
-        return FeedPub::NextSelector::Link.new("Next")
-      end
 
-      # find all elements with "next" in id or class or alt
-      # return the selector from the first one
-      selector = "[id*='next'], [class*='next'], [alt*='next']"
-      element = session.all(selector).first
+      final_selector = SELECTORS.find { |selector| selector.matches?(session) }
 
-      raise FeedPub::Error, "No next candidates found" unless element
-
-      final_selector =
-        if element["id"].present?
-          FeedPub::NextSelector::Attribute.new("[id='#{element["id"]}']")
-        elsif element["alt"].present?
-          FeedPub::NextSelector::Attribute.new("[alt='#{element["alt"]}']")
-        else
-          FeedPub::NextSelector::Attribute.new("[class='#{element["class"]}']")
-        end
+      raise FeedPub::Error, "No next candidates found" unless final_selector
 
       output.puts "final next selector: '#{final_selector}'"
 
