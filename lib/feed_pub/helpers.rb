6add958602677db05d4907e23016f3f48063a14a
Robert Fletcher <lobatifricha@gmail.com>
2025-04-13 19:24:46 -0700

    configure max pages (#43)

--- lib/feed_pub/helpers.rb
+++ lib/feed_pub/helpers.rb
@@ -2,13 +2,18 @@
 
 # helper methods for accessing configuration
 module FeedPub::Helpers
-  # return the configured output
-  def output
-    FeedPub::Configuration.output
-  end
-
   # return the configured file path
   def file_path
     FeedPub::Configuration.file_path
+  end
+
+  # return the configured max pages
+  def max_pages
+    FeedPub::Configuration.max_pages
+  end
+
+  # return the configured output
+  def output
+    FeedPub::Configuration.output
   end
 end
